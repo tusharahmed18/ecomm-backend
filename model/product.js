fdcecc672f09f73b1d45a66a41586d85351a21c6
tusharahmed18 <tusharahmedbadekhannavar@gmail.com>
2025-03-17 15:28:21 +0530

    done

---
+++ model/product.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    brand:{
+        type:String
+    },
+    stock:{
+        type:Number
+    },
+    image:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref: 'User'
+    }
+})
+
+const Product = mongoose.model('Product',productSchema);
+module.exports = {Product};
